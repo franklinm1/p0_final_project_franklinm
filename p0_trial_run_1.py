92e0d1ac3223f680094d6a3af7c4c51d92bf1b8a
franklinm1 <franklinm@berea.edu>
2017-11-23 15:09:33 -0500

    I added the header bar and imported the correct modules needed. This is the trail run file so I'm using it to test little things at a time

---
+++ p0_trial_run_1.py
@@ -0,0 +1,15 @@
+#####################################################################
+# Author: Megan Franklin
+# Username: franklim:
+#
+# Assignment: Project 0: Final Project
+#
+# Goals: This assignment is built upon using old code from past assignments and making them better by updating them
+#        with better code designs from what we've learned the whole semester.
+# Old Code Being Used: A5: The Game of Nim, and T1: Create your own Adventure
+#
+# Help From:
+######################################################################
+from time import sleep
+import random
+
